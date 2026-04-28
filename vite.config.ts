18a78541878cb48d98a0715c0f47470c2a052df6
AnushnaPatra05 <anushnapatra48@gmail.com>
2026-04-28 07:08:17 +0000

    Fix MIME Error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/AnushnaPatra05/",  
   server: {
     host: "::",
     port: 8080,
