3c3bbb6f0848629d8b74c1f37653c00326351213
AnushnaPatra05 <anushnapatra48@gmail.com>
2026-04-28 07:12:02 +0000

    Update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/AnushnaPatra05/",  
+  base: "/AnushnaPatra05.github.io/",  
   server: {
     host: "::",
     port: 8080,
