5ec44b9f6352fb5bffa7ad269c2443ecc48dbae2
AnushnaPatra05 <anushnapatra48@gmail.com>
2026-05-04 12:59:48 +0530

    Upload Certificate Files

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -8,9 +8,8 @@ export type Certificate = {
 };
 
 export const certificates: Certificate[] = [
-  { id: 1, title: "Certification Title 1", issuer: "Issuing Organization", date: "Month Year", file: "/certificates/cert-1.jpg", type: "image" },
-  { id: 2, title: "Certification Title 2", issuer: "Issuing Organization", date: "Month Year", file: "/certificates/cert-2.jpg", type: "image" },
-  { id: 3, title: "Certification Title 3", issuer: "Issuing Organization", date: "Month Year", file: "/certificates/cert-3.jpg", type: "image" },
-  { id: 4, title: "Certification Title 4", issuer: "Issuing Organization", date: "Month Year", file: "/certificates/cert-4.jpg", type: "image" },
-  { id: 5, title: "Certification Title 5", issuer: "Issuing Organization", date: "Month Year", file: "/certificates/cert-5.jpg", type: "image" },
-];
+  { id: 1, title: "ANGULAR", issuer: "UDEMY", date: "Month Year", file: "/certificates/UC-5aaa4584-76fa-4dae-a762-2e38313fdab1.pdf", type: "pdf" },
+  { id: 2, title: "AMAZON Q DEVELOPER", issuer: "UDEMY", date: "Month Year", file: "/certificates/Amazon%20Q%20Developer.jpg", type: "image" },
+  { id: 3, title: "TYPESCRIPT", issuer: "UDEMY", date: "Month Year", file: "certificates/UC-60aab9e2-56db-4050-b580-41203afb7654.pdf", type: "pdf" },
+  { id: 4, title: "AI + CHATGPT", issuer: "UDEMY", date: "Month Year", file: "/certificates/AI%2BChatGPT.pdf", type: "pdf" }
+]; 
